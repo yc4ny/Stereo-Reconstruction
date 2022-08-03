541927b83508f724f5ac3f74725a14838d1aa937
yc4ny <yhugestar@snu.ac.kr>
2022-08-04 00:51:19 +0900

    Concat Stereo + 3D Keypoint Visualization

--- 3d_keypoint_vis.py
+++ 3d_keypoint_vis.py
@@ -12,27 +12,30 @@ colors = [[1, 0, 0] for i in range(len(lines))]
 # line_set.lines = o3d.utility.Vector2iVector(lines)
 # line_set.colors = o3d.utility.Vector3dVector(colors)
 # o3d.visualization.draw_geometries([line_set])
+
 vis = o3d.visualization.Visualizer()
 vis.create_window()
 for j in range (0,851):
-    line_set = o3d.geometry.LineSet()
+    line_set = o3d.geometry.LineSet()   
     line_set.points = o3d.utility.Vector3dVector(dlt_keypoint[j])
     line_set.lines = o3d.utility.Vector2iVector(lines)
     line_set.colors = o3d.utility.Vector3dVector(colors)
     vis.add_geometry(line_set)
     ctr = vis.get_view_control()
     ctr.change_field_of_view(60.0)
-    ctr.set_front([ -0.33772111998121612, 0.27792873082682185, -0.89927752429359942 ])
-    ctr.set_up([ 0.17798370125143831, -0.91931835007481233, -0.35096377776711307 ])
-    ctr.set_zoom(1.2200000000000004)
+    ctr.set_front(  [ 0.60128001750265858, -0.38316853081412899, 0.70117345753083415 ])
+    ctr.set_up([ 0.072805529159330667, -0.84759919121340532, -0.52561865071381453 ])
+    ctr.set_zoom(2.0)
+    ctr.set_lookat([ 581.09208917074909, -370.90876972586221, 2657.3605125068325 ])
     vis.update_geometry(line_set)
     vis.poll_events()
     vis.update_renderer()
-    vis.capture_screen_image("3d_keypoint_img/" + str(j) + "_output.jpg")
+    vis.capture_screen_image("3dkeypoint_viz/4/" + str(j) + "_output.jpg")
     vis.remove_geometry(line_set)
 
 
 vis.destroy_window()
+
 
 
 # {
@@ -42,13 +45,53 @@ vis.destroy_window()
 # 	"trajectory" : 
 # 	[
 # 		{
-# 			"boundingbox_max" : [ 611.13276844923064, 388.49271414428762, 3193.9718037482467 ],
-# 			"boundingbox_min" : [ 32.803214651651871, -1217.1075027675793, 2218.3312428740846 ],
+# 			"boundingbox_max" : [ 610.12040994403708, 406.96750337211319, 3192.6585567195716 ],
+# 			"boundingbox_min" : [ 37.608649435065736, -1209.1806232983683, 2222.0660315545933 ],
 # 			"field_of_view" : 60.0,
-# 			"front" : [ -0.33772111998121612, 0.27792873082682185, -0.89927752429359942 ],
-# 			"lookat" : [ -123.92969744327998, -178.48855899714425, 2150.6651797712061 ],
-# 			"up" : [ 0.17798370125143831, -0.91931835007481233, -0.35096377776711307 ],
-# 			"zoom" : 1.2200000000000004
+# 			"front" : [ 0.60128001750265858, -0.38316853081412899, 0.70117345753083415 ],
+# 			"lookat" : [ 581.09208917074909, -370.90876972586221, 2657.3605125068325 ],
+# 			"up" : [ 0.072805529159330667, -0.84759919121340532, -0.52561865071381453 ],
+# 			"zoom" : 2.0
+# 		}
+# 	],
+# 	"version_major" : 1,
+# 	"version_minor" : 0
+# }
+
+# {
+# 	"class_name" : "ViewTrajectory",
+# 	"interval" : 29,
+# 	"is_loop" : false,
+# 	"trajectory" : 
+# 	[
+# 		{
+# 			"boundingbox_max" : [ 610.12040994403708, 406.96750337211319, 3192.6585567195716 ],
+# 			"boundingbox_min" : [ 37.608649435065736, -1209.1806232983683, 2222.0660315545933 ],
+# 			"field_of_view" : 60.0,
+# 			"front" : [ -0.921633374745366, -0.34427386941773164, 0.17907379873004997 ],
+# 			"lookat" : [ 581.09208917074909, -370.90876972586221, 2657.3605125068325 ],
+# 			"up" : [ 0.22129159103425, -0.84533125545739984, -0.486256208478973 ],
+# 			"zoom" : 2.0
+# 		}
+# 	],
+# 	"version_major" : 1,
+# 	"version_minor" : 0
+# }
+
+# {
+# 	"class_name" : "ViewTrajectory",
+# 	"interval" : 29,
+# 	"is_loop" : false,
+# 	"trajectory" : 
+# 	[
+# 		{
+# 			"boundingbox_max" : [ 610.12040994403708, 406.96750337211319, 3192.6585567195716 ],
+# 			"boundingbox_min" : [ 37.608649435065736, -1209.1806232983683, 2222.0660315545933 ],
+# 			"field_of_view" : 60.0,
+# 			"front" : [ 0.19536752216433198, -0.47264769902124026, 0.85932280540743033 ],
+# 			"lookat" : [ 581.09208917074909, -370.90876972586221, 2657.3605125068325 ],
+# 			"up" : [ 0.20241367107892361, -0.83790958072961408, -0.5068887849241448 ],
+# 			"zoom" : 2.0
 # 		}
 # 	],
 # 	"version_major" : 1,

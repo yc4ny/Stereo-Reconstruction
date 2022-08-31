dafc8bfb7ef7fc43f98efcdce431af77927e4991
yc4ny <yhugestar@snu.ac.kr>
2022-09-01 00:03:15 +0900

    Added Open3d visualizer for reconstructed checkerboard keypoints

--- renderer/visualizer_checkerboard_3d.py
+++ renderer/visualizer_checkerboard_3d.py
@@ -6,18 +6,18 @@ pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(keypoint)
 o3d.io.write_point_cloud('output_3d/nl_keypoints.ply', pcd)
 
-# vis = o3d.visualization.Visualizer()
-# vis.create_window()
+vis = o3d.visualization.Visualizer()
+vis.create_window()
 
-# # geometry is the point cloud used in your animaiton
-# geometry = o3d.geometry.PointCloud()
-# vis.add_geometry(geometry)
+# geometry is the point cloud used in your animaiton
+geometry = o3d.geometry.PointCloud()
+vis.add_geometry(geometry)
 
-# for i in range(keypoint.shape[0]):
-#     # now modify the points of your geometry
-#     # you can use whatever method suits you best, this is just an example
-#     geometry.points = keypoint[i]
-#     vis.update_geometry(geometry)
-#     vis.poll_events()
-#     vis.update_renderer()
+for i in range(keypoint.shape[0]):
+    # now modify the points of your geometry
+    # you can use whatever method suits you best, this is just an example
+    geometry.points = keypoint[i]
+    vis.update_geometry(geometry)
+    vis.poll_events()
+    vis.update_renderer()
     
